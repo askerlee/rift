2967b20cd2b66bf97970d6c7db5cb37d89085fc0
askerlee <shaohua@gmail.com>
2022-01-09 16:13:28 +0800

    change block1 chan to 152

--- model/IFNet.py
+++ model/IFNet.py
@@ -92,7 +92,7 @@ class IFNet(nn.Module):
         super(IFNet, self).__init__()
         self.trans_layer_idx = trans_layer_idx
         self.block0 = IFBlock('block0', 6,          c=240, apply_trans=(trans_layer_idx==0))
-        self.block1 = IFBlock('block1', 13+4,       c=150, apply_trans=(trans_layer_idx==1))
+        self.block1 = IFBlock('block1', 13+4,       c=152, apply_trans=(trans_layer_idx==1))
         self.block2 = IFBlock('block2', 13+4,       c=90,  apply_trans=(trans_layer_idx==2))
         self.block_tea = IFBlock('block_tea', 16+4, c=90,  apply_trans=(trans_layer_idx==2))
         self.contextnet = Contextnet()
