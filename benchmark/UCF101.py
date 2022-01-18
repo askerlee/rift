1c3aeef35a45c487b8fbac83257e0edc23cd7385
askerlee <shaohua@gmail.com>
2022-01-18 22:20:52 +0800

    ucf101

--- benchmark/UCF101.py
+++ benchmark/UCF101.py
@@ -11,8 +11,48 @@ from model.pytorch_msssim import ssim_matlab
 from model.RIFE import Model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = Model()
-model.load_model('train_log')
+parser = argparse.ArgumentParser()
+parser.add_argument('--trans', dest='trans_layer_indices', default="-1", type=str, 
+                    help='Which IFBlock to apply transformer (default: "-1", not to use transformer in any blocks)')
+parser.add_argument('--paper', action='store_true', help='Use the model in the RIFE paper')
+parser.add_argument('--oldmodel', dest='use_old_model', action='store_true', 
+                    help='Use the old model in the RIFE repo')
+parser.add_argument('--hd', action='store_true', help='Use newer HD model')
+parser.add_argument('--rife', dest='use_rife_settings', action='store_true', help='Use rife settings')
+parser.add_argument('--cp', type=str, default=None, help='Load checkpoint from this path')
+parser.add_argument('--count', type=int, default=-1, help='Evaluate on the first count images')
+parser.add_argument('--maskresweight', dest='mask_score_res_weight', default=-1, type=float, 
+                    help='Weight of the mask score residual connection')
+parser.add_argument('--multi', dest='multi', default=1, type=int, metavar='M', 
+                    help='Output M groups of flow (default: 1, single group)')
+parser.add_argument('--bn', dest='do_BN', action='store_true', 
+                    help='Use batchnorm between conv layers')
+
+args = parser.parse_args()
+args.trans_layer_indices = [ int(idx) for idx in args.trans_layer_indices.split(",") ]
+print(f"Args:\n{args}")
+
+if args.use_old_model:
+    model = Model(use_old_model=True)
+    model.load_model('rife_checkpoint/flownet.pth')
+elif args.paper:
+    model = Model(use_rife_settings=True)
+    model.load_model('rife_checkpoint/flownet.pth')
+elif args.hd:
+    from train_log.RIFE_HDv3 import Model
+    model = Model(use_rife_settings=True)
+    if not hasattr(model, 'version'):
+        model.version = 0
+    # -1: rank. If rank <= 0, remove "module" prefix from state_dict keys.
+    model.load_model('rife_hd_checkpoint/flownet.pth', -1)
+    print("Loaded 3.x/4.x HD model.")
+else:
+    model = Model(use_rife_settings=args.use_rife_settings, 
+                  mask_score_res_weight=args.mask_score_res_weight,
+                  multi=args.multi, do_BN=args.do_BN,
+                  trans_layer_indices=args.trans_layer_indices)
+    model.load_model(args.cp)
+
 model.eval()
 model.device()
 
