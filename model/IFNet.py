e79a9c61b413160071d13382cac492d2d4453834
askerlee <shaohua@gmail.com>
2022-01-20 17:09:49 +0800

    use multiflow as IFBlock input feat

--- model/IFNet.py
+++ model/IFNet.py
@@ -196,14 +196,14 @@ class IFNet(nn.Module):
         self.Ms = multi
         self.block0 =   IFBlock('block0',    6,    c=block_widths[0], img_chans=3, 
                                 multi=self.Ms[0],  mixfeat01=mixfeat01)
-        self.block1 =   IFBlock('block1',    13+4, c=block_widths[1], img_chans=6,  
+        self.block1 =   IFBlock('block1',    13+4*self.Ms[1], c=block_widths[1], img_chans=6,  
                                 multi=self.Ms[1],  mixfeat01=mixfeat01)
-        self.block2 =   IFBlock('block2',    13+4, c=block_widths[2], img_chans=6, 
+        self.block2 =   IFBlock('block2',    13+4*self.Ms[2], c=block_widths[2], img_chans=6, 
                                 multi=self.Ms[2],  mixfeat01=mixfeat01)
         # block_tea takes gt (the middle frame) as extra input. 
         # block_tea only outputs one group of flow, as it takes extra info and the single group of 
         # output flow is already quite accurate.
-        self.block_tea = IFBlock('block_tea', 16+4, c=block_widths[2],  img_chans=6, 
+        self.block_tea = IFBlock('block_tea', 16+4*self.Ms[2], c=block_widths[2],  img_chans=6, 
                                  multi=self.Ms[2], mixfeat01=mixfeat01)
         self.contextnet = Contextnet()
         # unet: 17 channels of input, 3 channels of output. Output is between 0 and 1.
@@ -249,10 +249,6 @@ class IFNet(nn.Module):
                 else:
                     stu_input = torch.cat((img0, img0_warped, img1, img1_warped, mask_score), 1)
 
-                # flow, flow_d: [16, 4*M, 224, 224]
-                # mask_score, mask_score_d:   [16, 1*M, 224, 224]
-                # flow_d, flow returned from an IFBlock is always of the size of the original image.
-                multiflow_d, multimask_score_d = stu_blocks[i](stu_input, flow, scale=scale_list[i])
                 if self.Ms[i-1] == self.Ms[i]:
                     multiflow_res       = multiflow
                     multimask_score_res = multimask_score
@@ -264,6 +260,10 @@ class IFNet(nn.Module):
                     multiflow_res       = torch.cat([ multiflow[:, :2*Mc], multiflow[:, 2*Mp:2*Mp+2*Mc] ], 1)
                     multimask_score_res = torch.cat([ multimask_score[:, :Mc], multimask_score[:, Mp:Mp+Mc], 
                                                       multimask_score[:, [-1]] ], 1)
+                # multiflow, multiflow_d: [16, 4*M, 224, 224]
+                # multimask_score, multimask_score_d:   [16, 1*M, 224, 224]
+                # multiflow, multimask_score returned from an IFBlock is always of the size of the original image.
+                multiflow_d, multimask_score_d = stu_blocks[i](stu_input, multiflow_res, scale=scale_list[i])
                 multiflow = multiflow_res + multiflow_d
                 multimask_score = multimask_score_d + multimask_score_res * self.mask_score_res_weight
             else:
@@ -292,12 +292,13 @@ class IFNet(nn.Module):
             else:
                 tea_input = torch.cat((img0, img0_warped, img1, img1_warped, mask_score, gt), 1)    
 
-            multiflow_d, multimask_score_d = self.block_tea(tea_input, flow, scale=1)
+            multiflow_d, multimask_score_d = self.block_tea(tea_input, multiflow, scale=1)
 
             # multiflow and multimask_score are from block2, 
             # which always have the same M as the teacher.
             multiflow_res       = multiflow
-            multimask_score_res = multimask_score                      
+            multimask_score_res = multimask_score
+            # Removing this residual connection makes the teacher perform much worse.                      
             multiflow_tea = multiflow_res + multiflow_d
             multimask_score_tea = multimask_score_d + multimask_score_res * self.mask_score_res_weight
             warped_img0_tea, warped_img1_tea = \
