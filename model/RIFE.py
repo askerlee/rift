b9fc7669e96a7c170101835042a9e5dae10b4218
askerlee <shaohua@gmail.com>
2022-02-04 16:25:01 +0800

    correct shift consistency offsets

--- model/RIFE.py
+++ model/RIFE.py
@@ -158,21 +158,23 @@ class Model:
         flow, mask, merged_img_list, flow_teacher, merged_teacher, loss_distill = self.flownet(torch.cat((imgs, gt), 1), scale_list=[4, 2, 1])
         if self.cons_shift_prob > 0:
             rand = random.random()
-            if rand < self.cons_shift_prob / 2:
-                img0a, gt2, mask, xy_shift = random_shift(img0, gt, 0)
-                img1a = img1
-            elif rand >= self.cons_shift_prob / 2 and rand < self.cons_shift_prob:
-                img1a, gt2, mask, xy_shift = random_shift(img1, gt, 1)
-                img0a = img0
-            imgs2 = torch.cat((img0a, img1a), 1)
-            if xy_shift is not None:
-                flow2, mask2, merged_img_list2, flow_teacher2, merged_teacher2, loss_distill2 = self.flownet(torch.cat((imgs2, gt2), 1), scale_list=[4, 2, 1])
-                consist_loss_stu = 0
-                # s enumerates all scales.
-                for s in range(len(flow)):
-                    consist_loss_stu += torch.abs(flow[s] + xy_shift - flow2[s])[mask].mean()
-                consist_loss_tea = torch.abs(flow_teacher + xy_shift - flow_teacher2)[mask].mean()
-                consist_loss = (consist_loss_stu / len(flow) + consist_loss_tea) / 2
+            if rand < self.cons_shift_prob:
+                if rand < self.cons_shift_prob / 2:
+                    img0a, gt2, mask, xy_shift = random_shift(img0, gt, 0)
+                    img1a = img1
+                elif rand >= self.cons_shift_prob / 2:
+                    img1a, gt2, mask, xy_shift = random_shift(img1, gt, 1)
+                    img0a = img0
+
+                imgs2 = torch.cat((img0a, img1a), 1)
+                if xy_shift is not None:
+                    flow2, mask2, merged_img_list2, flow_teacher2, merged_teacher2, loss_distill2 = self.flownet(torch.cat((imgs2, gt2), 1), scale_list=[4, 2, 1])
+                    consist_loss_stu = 0
+                    # s enumerates all scales.
+                    for s in range(len(flow)):
+                        consist_loss_stu += torch.abs(flow[s] + xy_shift - flow2[s])[mask].mean()
+                    consist_loss_tea = torch.abs(flow_teacher + xy_shift - flow_teacher2)[mask].mean()
+                    consist_loss = (consist_loss_stu / len(flow) + consist_loss_tea) / 2
             else:
                 consist_loss = 0
         else:
