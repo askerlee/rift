3149d3329a184d21364d533c2771fe27904f5be5
askerlee <shaohua@gmail.com>
2022-02-12 13:31:59 +0800

    update middlebury evaluation

--- benchmark/MiddleBury_Other.py
+++ benchmark/MiddleBury_Other.py
@@ -11,17 +11,48 @@ from model.pytorch_msssim import ssim_matlab
 from model.RIFE import Model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = Model()
-model.load_model('train_log')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--oldmodel', dest='use_old_model', action='store_true', 
+                    help='Use the old model in the RIFE repo')
+parser.add_argument('--hd', action='store_true', help='Use newer HD model')
+parser.add_argument('--cp', type=str, default=None, help='Load checkpoint from this path')
+parser.add_argument('--count', type=int, default=-1, help='Evaluate on the first count images')
+parser.add_argument('--multi', dest='multi', default="8,8,4", type=str, metavar='M', 
+                    help='Output M groups of flow')                      
+parser.add_argument('--ctxmergeflow', dest='ctx_use_merged_flow', action='store_true', 
+                    help='Use merged flow for contextnet.')
+
+args = parser.parse_args()
+args.multi = [ int(m) for m in args.multi.split(",") ]
+
+print(f"Args:\n{args}")
+
+if args.use_old_model:
+    model = Model(use_old_model=True)
+    model.load_model('rife_checkpoint/flownet.pth')
+elif args.hd:
+    from train_log.RIFE_HDv3 import Model
+    model = Model()
+    if not hasattr(model, 'version'):
+        model.version = 0
+    # -1: rank. If rank <= 0, remove "module" prefix from state_dict keys.
+    model.load_model('rife_hd_checkpoint/flownet.pth', -1)
+    print("Loaded 3.x/4.x HD model.")
+else:
+    model = Model(multi=args.multi, 
+                  ctx_use_merged_flow=args.ctx_use_merged_flow)
+    model.load_model(args.cp)
+
 model.eval()
 model.device()
 
 name = ['Beanbags', 'Dimetrodon', 'DogDance', 'Grove2', 'Grove3', 'Hydrangea', 'MiniCooper', 'RubberWhale', 'Urban2', 'Urban3', 'Venus', 'Walking']
 IE_list = []
 for i in name:
-    i0 = cv2.imread('other-data/{}/frame10.png'.format(i)).transpose(2, 0, 1) / 255.
-    i1 = cv2.imread('other-data/{}/frame11.png'.format(i)).transpose(2, 0, 1) / 255.
-    gt = cv2.imread('other-gt-interp/{}/frame10i11.png'.format(i)) 
+    i0 = cv2.imread('middlebury/other-data/{}/frame10.png'.format(i)).transpose(2, 0, 1) / 255.
+    i1 = cv2.imread('middlebury/other-data/{}/frame11.png'.format(i)).transpose(2, 0, 1) / 255.
+    gt = cv2.imread('middlebury/other-gt-interp/{}/frame10i11.png'.format(i)) 
     h, w = i0.shape[1], i0.shape[2]
     imgs = torch.zeros([1, 6, 480, 640]).to(device)
     ph = (480 - h) // 2
