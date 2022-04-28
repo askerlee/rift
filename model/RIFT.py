852eca5307351c3b34876e89d793ac812a327cac
askerlee <shaohua@gmail.com>
2022-04-28 15:10:27 +0800

    fix stopgrad prob bug

--- model/RIFT.py
+++ model/RIFT.py
@@ -37,6 +37,7 @@ class RIFT:
                  grad_clip=-1, 
                  distill_loss_weight=0.02, 
                  smooth_loss_weight=0.001,
+                 stopgrad_prob=0.3,
                  multi=(8,8,4), 
                  weight_decay=1e-3,
                  consistency_args={},
@@ -49,7 +50,7 @@ class RIFT:
         if use_old_model:
             self.flownet = IFNet_rife()
         else:
-            self.flownet = IFNet(multi, esti_sofi)
+            self.flownet = IFNet(multi, esti_sofi, stopgrad_prob)
         self.device()
 
         conv_param_groups, trans_param_groups = [], []
