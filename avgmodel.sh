182ed713a0c193b2cae2ea765f3df510d6211dd4
askerlee <shaohua@gmail.com>
2022-05-07 13:01:25 +0800

    add avgmodel.sh

--- avgmodel.sh
+++ avgmodel.sh
@@ -5,4 +5,5 @@ set -gx glob $argv[2]
 set -gx globpat (string replace -a 'x' '*' $glob)
 rm -f $timestamp-ep$glob.pth
 python3 ~/pytorch-image-models/avg_checkpoints.py --input ../rift/checkpoints/$timestamp/ --filter ep$globpat --no-sort --no-use-ema --output $timestamp-ep$glob.pth
-python3 ../craft/evaluate.py --dataset chairs --sofi --model $timestamp-ep$glob.pth
+cd ../craft/
+python3 evaluate.py --dataset chairs --sofi --model $timestamp-ep$glob.pth
