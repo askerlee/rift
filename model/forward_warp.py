0191e14ad85d82e07706018980bc29fc296a64fb
askerlee <shaohua@gmail.com>
2022-05-08 22:48:12 +0800

    reimplement forward_warp

--- model/forward_warp.py
+++ model/forward_warp.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import inspect
-from visgraph import make_dot
+#from model.visgraph import make_dot
 from warp import backwarp
 
 def mesh_grid(B, H, W):
